67d8a3e172e9391096bb7940d621ead95fd393ac
Dmitry <43668928+dkskv@users.noreply.github.com>
2022-03-12 21:52:13 +0300

    style: исправил форматирование

--- src/download.ts
+++ src/download.ts
@@ -1,17 +1,16 @@
 export default function download(data: string, name: string, type?: string) {
-    const file = new Blob([data], { type });
-  
-    const a = document.createElement("a");
-    const url = URL.createObjectURL(file);
-  
-    a.href = url;
-    a.download = name;
-    document.body.appendChild(a);
-    a.click();
-  
-    setTimeout(() => {
-      document.body.removeChild(a);
-      URL.revokeObjectURL(url);
-    }, 0);
-  }
-  
+  const file = new Blob([data], { type });
+
+  const a = document.createElement("a");
+  const url = URL.createObjectURL(file);
+
+  a.href = url;
+  a.download = name;
+  document.body.appendChild(a);
+  a.click();
+
+  setTimeout(() => {
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+  }, 0);
+}
