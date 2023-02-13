99f9c3498ffc579c0a1df7428f8a1f68e385593a
Dmitry Kuskov <dmtrkskv@gmail.com>
2023-02-13 20:45:24 +0300

    chore: разделил директории assets и build

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,27 @@
-const path = require('path');
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: "./src/index.ts",
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: [".ts", ".js"],
   },
   output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'build'),
+    filename: "index.js",
+    path: path.resolve(__dirname, "build"),
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "assets" }],
+    }),
+  ],
 };
